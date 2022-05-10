b52142e1f6335839f9f5dc70cbc4ceca8c3e2df3
TheBanusco10 <thebanusco10@gmail.com>
2022-05-10 21:54:45 +0200

    Some methods in vuex

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -24,6 +24,7 @@ export default {
 
         const filesUploaded = ref([]);
 
+        // TODO uploadFile in vuex
         const uploadFile = async (evt) => {
             const files = [...evt.target.files];
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    userFiles: []
   },
   getters: {
+    getUserFiles: (state) => {
+      return state.userFiles;
+    }
   },
   mutations: {
+    getUserFiles: ({commit}) => {
+      commit.commit('getUserFiles');
+    }
   },
   actions: {
+    getUserFiles: async ({state}) => {
+      const res = await fetch('http://localhost:3000/api/file');
+      const data = await res.json();
+
+      state.userFiles = data.files;
+  }
   },
   modules: {
   }
